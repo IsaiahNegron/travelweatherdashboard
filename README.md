52fa6b26808a6e740de8e1a86159c7895d7e983f
Isaiah Negron <isaiahanegron@gmail.com>
2021-10-10 11:50:42 -0500

    fixed readme

--- README.md
+++ README.md
@@ -1,32 +1,40 @@
-# travelweatherdashboard
+# Weather Dashboard
 
-# 06 Server-Side APIs: Weather Dashboard
+  ![badge](https://img.shields.io/badge/license-MIT-important)
 
-Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+  ## Description
 
-Use the [OpenWeather One Call API](https://openweathermap.org/api/one-call-api) to retrieve weather data for cities. Read through the documentation for setup and usage instructions. You will use `localStorage` to store any persistent data.
+  we had to create a weather dashboard with current and 5 day weather status'
 
-## User Story
+  ## Table of Contents
+  - [Description](#Description)
+  - [Installation Instructions](#Installation)
+  - [Usage](#Usage)
+  - [Contributions](#Contributions)
+  - [Testing](#Tests)
+  - [License](#License)
+  - [Github](#Github)
+  - [Contact](#Contact)
 
-```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-```
+  ## Installation Instructions
+  n/a
 
-## Acceptance Criteria
+  ## Usage
+  to check the weather in a desired location
 
-```
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-```
+  ## Contributions
+  Peers and KU full-stack bootcamp. (this project wasn't fully completed)
 
+  ## Testing
+  n/a
+
+  ## License
+  ![badge](https://img.shields.io/badge/license-MIT-important)
+  <br>
+  Permission to use this application is granted under the MIT License. <https://opensource.org/licenses/MIT>
+
+  ## Github Username
+  Lets Connect: Isaiah Negron
+
+  ## Contact
+  If you have any questions feel free to contact me at <a href='mailto:isaiahanegron@gmail.com'>isaiahanegron@gmail.com</a>
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,15 @@
 var searchButtonEl = document.querySelector(".btn");
-var cityNameInput = document.querySelector("#city-search");
+var cityNameInput = document.querySelector("#search");
 var icon = document.querySelector(".icon");
 var humidity = document.querySelector(".humidity");
 var temp = document.querySelector(".temp");
 var wind = document.querySelector(".wind");
 var uvIndex = document.querySelector(".index");
+var geoBaseUrl = "http://api.openweathermap.org/geo/1.0/direct?q=";
+var geoEndUrl = "&limit=5&appid=d1d4775eb8ccb43358d90f15b59711df";
+
+
+
 
 //this is the button function!!
 // var formSubmitHandler = function(event) {
@@ -22,17 +27,36 @@ https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hou
 
 // searchButtonEl.addEventListener("click",function(){
 //     fetch("https://api.openweathermap.org/data/2.5/weather?q="+ cityNameInput.value +"&units=imperial&appid=d1d4775eb8ccb43358d90f15b59711df")
-    // .then(response => response.json())
-    // .then(data => console.log(data))
+//     .then(response => response.json())
+//     .then(data => console.log(data.coord))
 
-    // .catch(err => alert("Wrong City Name, Check Your Spelling"))
+//     .catch(err => alert("Wrong City Name, Check Your Spelling"))
 
 // });
 
+function getWeatherInfo(cityNameInput){
+
+  fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + cityNameInput + "&limit=5&appid=d1d4775eb8ccb43358d90f15b59711df")
+  .then(response => response.json())
+  .then(data => console.log(data[0].lat))
+
+  .catch(err => alert("wrong info"));
+
+}
+
+searchButtonEl.addEventListener("click", getWeatherInfo(cityNameInput));
+
+
+
+// async function getCoords(){
+//     var response = await fetch("https://api.openweathermap.org/data/2.5/weather?q="+ cityNameInput.value +"&units=imperial&appid=d1d4775eb8ccb43358d90f15b59711df");
+//     var lat = await data[0].lat;
+// };
+
 // searchButtonEl.addEventListener('click', function(){
-//     $.getJSON("http://api.openweathermap.org/geo/1.0/direct?q=" + cityNameInput.value + "&limit=5&appid=d1d4775eb8ccb43358d90f15b59711df",
-//     function(data){
-//         console.log(data);
+    // $.getJSON("http://api.openweathermap.org/geo/1.0/direct?q=" + cityNameInput.value + "&limit=5&appid=d1d4775eb8ccb43358d90f15b59711df",
+    // function(data){
+    //     console.log(data);
         
 
 //         getWeatherInfo(data)
@@ -49,29 +73,3 @@ https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hou
 
 //     .catch(err => alert("Wrong City Name, Check Your Spelling"))
 // };
-
-
-
-
-    
-
-
-
-// $.getJSON(
-//     "https://api.openweathermap.org/data/2.5/weather?q="+ cityName + "&units=imperial&appid=d1d4775eb8ccb43358d90f15b59711df",
-//     function(data){
-
-        // var icon =  "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-        // var temp = data.main.temp + ("°F");
-        // var humidity = data.main.humidity + ("%");
-        // var wind = data.wind.speed + (" MPH");
-        // // var index = data.
-    
-        // $(".icon").attr("src", icon);
-        // $(".temp").append(temp);
-        // $(".humidity").append(humidity);
-        // $(".wind").append(wind);
-
-//     }
-// );
-
